e965f8d5ee11c4d588941a15a6fd69d8636963f9
butrintzeqiri6 <butrintzeqiri6@gmail.com>
2026-04-11 11:41:11 +0200

    horizont

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -5,6 +5,7 @@ const students = [
     {name: 'Bledar', lastname: 'Abdili', age: '15'},
     {name: 'Naron', lastname: 'Emurllai', age: '15'},
     {name: 'Viola', lastname: 'Ismaili', age: '14'},
+    {name: 'Butrint', lastname: 'Zeqiri', age: '15'}
 ]
 
 const ListScreen = () => {
@@ -12,6 +13,9 @@ const ListScreen = () => {
         <View>
             <Text style={styles.textStyle}>List of students:</Text>
             <FlatList 
+            horizontal={true}
+            showsHorizontalScrollIndicator={true}
+            keyExtractor={student => student.name}
                 data={students}
                 renderItem={({item}) => {
                     return <Text>{item.name} {item.lastname} - {item.age}</Text>
