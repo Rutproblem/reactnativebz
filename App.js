c800c2b55fe002c869f8625f0695b57a95cccc6d
butrintzeqiri6 <butrintzeqiri6@gmail.com>
2026-04-11 10:52:49 +0200

    sgr

--- App.js
+++ App.js
@@ -3,14 +3,24 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ExerciseScreen from './screens/ExerciseScreen';
+import MainScreen from './screens/MainScreen';
+import ListScreen from './screens/ListScreen';
+import TestScreen from './screens/TestScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={ExerciseScreen}></Stack.Screen>
+      <Stack.Navigator
+      initialRouteName='List'
+      screenOptions={{
+        title: 'App'
+      }}>
+        <Stack.Screen name="Home" component={MainScreen}></Stack.Screen>
+        <Stack.Screen name="Test" component={TestScreen}></Stack.Screen>
+        <Stack.Screen name="Exercise" component={ExerciseScreen}></Stack.Screen>
+        <Stack.Screen name="List" component={ListScreen}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
