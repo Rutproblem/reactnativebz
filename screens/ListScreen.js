26ffa8f5a8b313b0ef48edf4296666a43653135b
butrintzeqiri6 <butrintzeqiri6@gmail.com>
2026-04-11 11:37:51 +0200

    kolurz

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { Text, View, StyleSheet, FlatList } from "react-native";
 
+const students = [
+    {name: 'Bledar', lastname: 'Abdili', age: '15'},
+    {name: 'Naron', lastname: 'Emurllai', age: '15'},
+    {name: 'Viola', lastname: 'Ismaili', age: '14'},
+]
+
 const ListScreen = () => {
     return (
-        <Text>List Screen</Text>
+        <View>
+            <Text style={styles.textStyle}>List of students:</Text>
+            <FlatList 
+                data={students}
+                renderItem={({item}) => {
+                    return <Text>{item.name} {item.lastname} - {item.age}</Text>
+                }}
+            />
+        </View>
     )
 }
 
-const styles = StyleSheet.create()
+const styles = StyleSheet.create({
+    textStyle: {
+        color: "blue"
+    }
+})
 
 export default ListScreen;
